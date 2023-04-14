prepping for overhaul clean up and fixed KL issue + implemented KL budget but still not working great

legacy_code/decoder_utils_delta.py
@@ -13,7 +13,7 @@ from logger import log
 from model_utils import *
 import ipdb
 
-class IEGMN_Bidirectional_Layer(nn.Module):
+class IEGMN_Bidirectional_Delta_Layer(nn.Module):
     def __init__(
             self,
             orig_invar_feats_dim_h, #orig_h_feats_dim,
@@ -51,7 +51,7 @@ class IEGMN_Bidirectional_Layer(nn.Module):
             B_input_edge_feats_dim = None, #rec_input_edge_feats_dim,
     ):
 
-        super(IEGMN_Bidirectional_Layer, self).__init__()
+        super(IEGMN_Bidirectional_Delta_Layer, self).__init__()
         
         # self.fine_tune = fine_tune
         self.cross_msgs = cross_msgs
@@ -83,7 +83,7 @@ class IEGMN_Bidirectional_Layer(nn.Module):
         # EDGES
         A_edge_mlp_input_dim = (invar_feats_dim_h * 2) #+ edge_feats_dim #! removed the edge features
         if self.use_dist_in_layers:# and self.A_evolve: #TRUE and TRUE
-            A_edge_mlp_input_dim += len(self.all_sigmas_dist)
+            A_edge_mlp_input_dim += 2*len(self.all_sigmas_dist) # 2 is for the reference
         # if self.standard_norm_order: # TRUE
         self.A_edge_mlp = nn.Sequential(
             nn.Linear(A_edge_mlp_input_dim, self.out_feats_dim_h),
@@ -221,8 +221,12 @@ class IEGMN_Bidirectional_Layer(nn.Module):
             x_rel_mag = edges.data['x_rel_cc'] ** 2
             x_rel_mag = torch.sum(x_rel_mag, dim=1, keepdim=True)
             x_rel_mag = torch.cat([torch.exp(-x_rel_mag / sigma) for sigma in self.all_sigmas_dist], dim=-1)
+
+            x_ref_mag = edges.data['x_rel_cc'] ** 2
+            x_ref_mag = torch.sum(x_ref_mag, dim=1, keepdim=True)
+            x_ref_mag = torch.cat([torch.exp(-x_ref_mag / sigma) for sigma in self.all_sigmas_dist], dim=-1)
             return {'msg_cc': self.A_edge_mlp(
-                torch.cat([edges.src['feat_cc'], edges.dst['feat_cc'], x_rel_mag], dim=1))} # edges.data['feat'], x_rel_mag], dim=1))} # operates with edge features in it and node features
+                torch.cat([edges.src['feat_cc'], edges.dst['feat_cc'], x_rel_mag, x_ref_mag], dim=1))}
         else:
             return {
                 'msg_cc': self.A_edge_mlp(torch.cat([edges.src['feat_cc'], edges.dst['feat_cc'], edges.data['feat']], dim=1))} #TODO: fix edge data what signal do we use?
@@ -254,10 +258,12 @@ class IEGMN_Bidirectional_Layer(nn.Module):
             # ipdb.set_trace()
             A_graph.ndata['x_now_cc'] = coords_A
             A_graph.ndata['feat_cc'] = h_feats_A  # first time set here
+            reference_A = A_graph.ndata['reference_point']
             print("[DEC single] forward input Coords A", torch.min(coords_A).item(), torch.max(coords_A).item(), torch.norm(coords_A, 2).item())
 
             # if self.A_evolve:
             A_graph.apply_edges(fn.u_sub_v('x_now_cc', 'x_now_cc', 'x_rel_cc'))  # x_i - x_j
+            A_graph.apply_edges(fn.u_sub_v('x_now_cc', 'reference_point', 'x_rel_ref'))
 
             A_graph.apply_edges(self.apply_edges_A)  ## i->j edge:  [h_i h_j] phi^e edge_mlp
             # Equation 1 message passing to create 'msg'
@@ -274,7 +280,8 @@ class IEGMN_Bidirectional_Layer(nn.Module):
             # if self.A_evolve:
             A_graph.update_all(self.update_x_moment_A, fn.mean('m_cc', 'x_update_cc')) # phi_x coord_mlp
             # Inspired by https://arxiv.org/pdf/2108.10521.pdf we use original X and not only graph.ndata['x_now']
-            x_evolved_A = self.x_connection_init * orig_coords_A + (1. - self.x_connection_init) * A_graph.ndata['x_now_cc'] + A_graph.ndata['x_update_cc']
+            x_evolved_A_delta = self.x_connection_init * (orig_coords_A-reference_A) + (1. - self.x_connection_init) * (A_graph.ndata['x_now_cc']-reference_A) + A_graph.ndata['x_update_cc']
+            x_evolved_A = reference_A + x_evolved_A_delta
             # else:
             #     x_evolved_A = coords_A
             print("   [DEC MPNN] X A", torch.min(x_evolved_A).item(), torch.max(x_evolved_A).item(), torch.norm(x_evolved_A, 2).item())
@@ -336,13 +343,15 @@ class IEGMN_Bidirectional_Layer(nn.Module):
             return x_evolved_A, node_upd_A, None, None, trajectory, geom_loss
 
     def forward(self, A_graph, B_graph, coords_A, h_feats_A, original_A_node_features, orig_coords_A,
-                coords_B, h_feats_B, original_B_node_features, orig_coords_B, mask, geometry_graph_A = None, geometry_graph_B = None, mpnn_only = False):
+                coords_B, h_feats_B, original_B_node_features, orig_coords_B, mask,
+                geometry_graph_A = None, geometry_graph_B = None, mpnn_only = False):
         if mpnn_only or B_graph is None: return self.single_forward(A_graph, coords_A, h_feats_A, original_A_node_features, orig_coords_A, geometry_graph_A, mask)
         with A_graph.local_scope() and B_graph.local_scope():
             A_graph.ndata['x_now_cc'] = coords_A
             B_graph.ndata['x_now_cc'] = coords_B
             A_graph.ndata['feat_cc'] = h_feats_A  # first time set here
             B_graph.ndata['feat_cc'] = h_feats_B
+            reference_A = A_graph.ndata['reference_point']
             print("[DEC start] forward input Coords A", torch.min(coords_A).item(), torch.max(coords_A).item())
             print("[DEC start] forward input Coords B", torch.min(coords_B).item(), torch.max(coords_B).item())
             print("[DEC start] forward input feat A", torch.min(h_feats_A).item(), torch.max(h_feats_A).item())
@@ -350,6 +359,7 @@ class IEGMN_Bidirectional_Layer(nn.Module):
 
             # if self.A_evolve:
             A_graph.apply_edges(fn.u_sub_v('x_now_cc', 'x_now_cc', 'x_rel_cc'))  # x_i - x_j
+            A_graph.apply_edges(fn.u_sub_v('x_now_cc', 'reference_point', 'x_rel_ref'))
             # if self.B_evolve:
             B_graph.apply_edges(fn.u_sub_v('x_now_cc', 'x_now_cc', 'x_rel_cc'))
 
@@ -375,7 +385,9 @@ class IEGMN_Bidirectional_Layer(nn.Module):
             # if self.A_evolve:
             A_graph.update_all(self.update_x_moment_A, fn.mean('m_cc', 'x_update_cc')) # phi_x coord_mlp
             # Inspired by https://arxiv.org/pdf/2108.10521.pdf we use original X and not only graph.ndata['x_now']
-            x_evolved_A = self.x_connection_init * orig_coords_A + (1. - self.x_connection_init) * A_graph.ndata['x_now_cc'] + A_graph.ndata['x_update_cc']
+            # x_evolved_A = self.x_connection_init * orig_coords_A + (1. - self.x_connection_init) * A_graph.ndata['x_now_cc'] + A_graph.ndata['x_update_cc']
+            x_evolved_A_delta = self.x_connection_init * (orig_coords_A-reference_A) + (1. - self.x_connection_init) * (A_graph.ndata['x_now_cc']-reference_A) + A_graph.ndata['x_update_cc']
+            x_evolved_A = reference_A + x_evolved_A_delta
             # if self.B_evolve:
             B_graph.update_all(self.update_x_moment_B, fn.mean('m_cc', 'x_update_cc'))
             print("            [DEC MPNN] orig B ", torch.min(orig_coords_B).item(), torch.max(orig_coords_B).item(), torch.norm(orig_coords_B, 2).item())
@@ -485,7 +497,7 @@ class IEGMN_Bidirectional_Layer(nn.Module):
         return "Decoder IEGMN Custom Layer " + str(self.__dict__)
 
 
-class IEGMN_Bidirectional(nn.Module):
+class IEGMN_Bidirectional_Delta(nn.Module):
     # def __init__(self, n_lays, debug, device, shared_layers, noise_decay_rate, cross_msgs, noise_initial,
     #              use_edge_features_in_gmn, use_mean_node_features, atom_emb_dim, iegmn_lay_hid_dim, num_att_heads,
     #              dropout, nonlin, leakyrelu_neg_slope, random_vec_dim=0, random_vec_std=1, use_scalar_features=True,
@@ -504,7 +516,7 @@ class IEGMN_Bidirectional(nn.Module):
                  use_edge_features_in_gmn, use_mean_node_features, atom_emb_dim, latent_dim, coord_F_dim,
                  dropout, nonlin, leakyrelu_neg_slope, random_vec_dim=0, random_vec_std=1, use_scalar_features=True,
                  num_A_feats=None, save_trajectories=False, weight_sharing = True, conditional_mask=False, **kwargs):
-        super(IEGMN_Bidirectional, self).__init__()
+        super(IEGMN_Bidirectional_Delta, self).__init__()
         self.debug = debug
         self.cross_msgs = cross_msgs
         self.device = device
@@ -544,7 +556,7 @@ class IEGMN_Bidirectional(nn.Module):
         self.iegmn_layers = nn.ModuleList()
         # Create First Layer
         self.iegmn_layers.append(
-            IEGMN_Bidirectional_Layer(orig_invar_feats_dim_h=input_node_feats_dim,
+            IEGMN_Bidirectional_Delta_Layer(orig_invar_feats_dim_h=input_node_feats_dim,
                         invar_feats_dim_h= latent_dim , #TODO we start at D here input_node_feats_dim,
                         out_feats_dim_h=latent_dim,
                         nonlin=nonlin,
@@ -558,7 +570,7 @@ class IEGMN_Bidirectional(nn.Module):
 
         # Create N - 1 layers
         if shared_layers:
-            interm_lay = IEGMN_Bidirectional_Layer(orig_invar_feats_dim_h=input_node_feats_dim,
+            interm_lay = IEGMN_Bidirectional_Delta_Layer(orig_invar_feats_dim_h=input_node_feats_dim,
                                      invar_feats_dim_h=latent_dim,
                                      out_feats_dim_h=latent_dim,
                                      cross_msgs=self.cross_msgs,
@@ -575,7 +587,7 @@ class IEGMN_Bidirectional(nn.Module):
             for layer_idx in range(1, n_lays):
                 debug_this_layer = debug if n_lays - 1 == layer_idx else False
                 self.iegmn_layers.append(
-                    IEGMN_Bidirectional_Layer(orig_invar_feats_dim_h=input_node_feats_dim,
+                    IEGMN_Bidirectional_Delta_Layer(orig_invar_feats_dim_h=input_node_feats_dim,
                                 invar_feats_dim_h=latent_dim,
                                 out_feats_dim_h=latent_dim,
                                 cross_msgs=self.cross_msgs,
@@ -701,29 +713,7 @@ class IEGMN_Bidirectional(nn.Module):
                                 geometry_graph_B=geometry_graph_B,
                                 mpnn_only=mpnn_only
                                 )
-            # #! Trying teacher forcing at each layer
-            # coords_A, \
-            # h_feats_A, \
-            # cat, \
-            # dog, trajectory, geom_loss = layer(A_graph=A_graph,
-            #                     B_graph=B_graph,
-            #                     coords_A=coords_A,
-            #                     h_feats_A=h_feats_A,
-            #                     original_A_node_features=original_A_node_features,
-            #                     orig_coords_A=orig_coords_A,
-            #                     coords_B=coords_B,
-            #                     h_feats_B=h_feats_B,
-            #                     original_B_node_features=original_B_node_features,
-            #                     orig_coords_B=orig_coords_B,
-            #                     mask=mask,
-            #                     geometry_graph_A=geometry_graph_A,
-            #                     geometry_graph_B=geometry_graph_B,
-            #                     mpnn_only=mpnn_only
-            #                     )
             print("[Decoder MPNN] geom loss of IEGMN layer", geom_loss.item())
-            if geom_loss.item() > 1e10:
-                # ipdb.set_trace()
-                print("Distance blow up")
             geom_losses = geom_losses + geom_loss
             full_trajectory.extend(trajectory)
         return coords_A, h_feats_A, coords_B, h_feats_B, geom_losses, full_trajectory

legacy_code/ecn_3d_layers.py
@@ -314,7 +314,7 @@ class Pooling_3D_Layer(nn.Module):
             trajectory = []
             # Done: Set up the following regularization for CG only
             if self.save_trajectories: trajectory.append(x_evolved_A.detach().cpu()[-N,:])
-            if self.loss_geometry_regularization:
+            if False and self.loss_geometry_regularization:
                 x_evolved_A_coarse = x_evolved_A[-N:,:]
                 src, dst = geometry_graph_A.edges()
                 src = src.long()
@@ -330,7 +330,7 @@ class Pooling_3D_Layer(nn.Module):
                 geom_loss += torch.sum((d_squared - geometry_graph_B.edata['feat'] ** 2) ** 2)
             else:
                 geom_loss = 0
-            if self.geometry_regularization:
+            if False and self.geometry_regularization:
                 x_evolved_A_coarse = x_evolved_A[-N:,:]
                 src, dst = geometry_graph_A.edges()
                 src = src.long()
@@ -761,6 +761,10 @@ class Coarse_Grain_3DLayer(nn.Module):
             v_evolved_B = self.skip_weight_v * self.B_vn_mlp_4(v_evolved_A_input) + (1. - self.skip_weight_v) * v_B
             print("[Encoder Coarse] ecn output V A", torch.min(v_evolved_A).item(), torch.max(v_evolved_A).item())
             print("[Encoder Coarse] ecn output V B", torch.min(v_evolved_B).item(), torch.max(v_evolved_B).item())
+            if torch.isnan(v_evolved_A).any().item():
+                print("Encoder NaN")
+                ipdb.set_trace()
+                nans_here = self.A_vn_mlp_4(v_evolved_A_input)
             return v_evolved_A, h_A, v_evolved_B, h_B
     #         # Equation 3: coordinate update
     #         A_graph.update_all(self.update_x_moment_A, fn.mean('m', 'x_update')) # phi_x coord_mlp

legacy_code/train_vae_full.py
@@ -43,7 +43,7 @@ ecn_model_params = {
 #   "B_evolve": True,
 #   "B_no_softmax": False,
 #   "A_no_softmax": False,
-  "n_lays": 3,  # 5 in  good run
+  "n_lays": 3,  # 3 5 in  good run
   "debug": False,
   "shared_layers": False, # False in good run
   "noise_decay_rate": 0.5,
@@ -79,10 +79,10 @@ drugs_path = geom_path + "drugs/"
 print("Loading QM9...")
 # with open(geom_path + "qm9_safe_v2.pickle", 'rb') as f:
 #     qm9 = pickle.load(f)
-batch_size = 50 #25
-train_limit = 2500 #100
+batch_size = 25#50 #25
+train_limit = 100#2500 #100
 train_loader, train_data = load_torsional_data(batch_size = batch_size, mode= 'train', limit_mols = train_limit)
-val_limit = 50 #25
+val_limit = 25#50 #25
 val_loader, val_data = load_torsional_data(batch_size = batch_size, mode= 'val', limit_mols = val_limit)
 print("Loading QM9 --> Done")
 
@@ -131,8 +131,8 @@ if __name__ =="__main__":
   torch.autograd.set_detect_anomaly(True)
   # train_loss_log_name = "torsional_diffusion_test_geomol_1000_minpostclamp" + "_train"
   # val_loss_log_name = "torsional_diffusion_test_geomol2_1000_minpostclamp" + "_val"
-  train_loss_log_name = "n1_ref_test_dist_large_soft" + "_train"
-  val_loss_log_name = "n1_ref_test_dist_large_soft" + "_val"
+  train_loss_log_name = "n1_ref_test_dist_no_clamp_budget52clamp2" + "_train"
+  val_loss_log_name = "n1_ref_test_dist_no_clamp_budget52clamp2" + "_val"
   train_loss_log_total, val_loss_log_total = [], []
   for epoch in range(10000):
     print("\n\n\n\n\nEpoch", epoch)

legacy_code/train_vae_full_delta.py
@@ -131,8 +131,8 @@ if __name__ =="__main__":
   torch.autograd.set_detect_anomaly(True)
   # train_loss_log_name = "torsional_diffusion_test_geomol_1000_minpostclamp" + "_train"
   # val_loss_log_name = "torsional_diffusion_test_geomol2_1000_minpostclamp" + "_val"
-  train_loss_log_name = "n1_ref_test_dist_delta" + "_train"
-  val_loss_log_name = "n1_ref_test_dist_delta" + "_val"
+  train_loss_log_name = "n1_ref_test_dist_delta_ref" + "_train"
+  val_loss_log_name = "n1_ref_test_dist_delta_ref" + "_val"
   train_loss_log_total, val_loss_log_total = [], []
   for epoch in range(10000):
     print("\n\n\n\n\nEpoch", epoch)

legacy_code/vae.py
@@ -20,8 +20,8 @@ class VAE(nn.Module):
         self.kl_v_beta = 1#e-4 2.5
         self.kl_h_beta = 0#1e-4
         self.kl_reg_beta = 1
-        self.align_kabsch_weight = 20
-        self.ar_rmsd_weight = 10 #1
+        self.align_kabsch_weight = 1 #20
+        self.ar_rmsd_weight = 1 #10 #1
         self.mse = nn.MSELoss()
         # self.mse_sum= nn.MSELoss(reduction='sum')
         self.device = device
@@ -56,8 +56,8 @@ class VAE(nn.Module):
             dst = dst.long()
             generated_coords = generated_mol.ndata['x_cc']
             d_squared = torch.sum((generated_coords[src] - generated_coords[dst]) ** 2, dim=1)
-            # geom_loss.append(1/len(src) * torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2)) #TODO: scaling hurt performance
-            geom_loss.append(torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
+            geom_loss.append(1/len(src) * torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2)) #TODO: scaling hurt performance
+            # geom_loss.append(torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
         print("[Distance Loss]", geom_loss)
         return torch.mean(torch.tensor(geom_loss))
 
@@ -68,7 +68,7 @@ class VAE(nn.Module):
         print("KL prior reg kl", kl_v_reg)
     
         kl_loss = self.kl_v_beta*kl_v + self.kl_h_beta*kl_h + self.kl_reg_beta*kl_v_reg
-        if step < 5:#0:
+        if step < 0:#50:
             kl_loss = 0.1*kl_loss
         
         x_cc, h_cc = channel_selection_info
@@ -120,7 +120,7 @@ class VAE(nn.Module):
         rdkit_loss = sum([x.ndata['rdkit_loss'][0] for x in dgl.unbatch(rdkit_reference)])
         distance_lambda = 1
         distance_loss = distance_lambda*self.distance_loss(generated_molecule, geometry_graph)
-        ar_dist_lambda = 1
+        ar_dist_lambda = 0.3 #1
         ar_dist_loss = ar_dist_lambda*ar_dist_loss
         print("[Loss Func] distance", distance_loss)
         print("[Loss Func] ar distance", ar_dist_loss)
@@ -131,6 +131,7 @@ class VAE(nn.Module):
 
     def std(self, input):
         return 1e-12 + torch.exp(input / 2)
+        #  return 1e-12 + F.softplus(input / 2)
 
     def align(self, source, target):
         # Rot, trans = rigid_transform_Kabsch_3D_torch(input.T, target.T)
@@ -167,7 +168,7 @@ class VAE(nn.Module):
         for chunk in chunks:
             sub_loss = self.rmsd(coords[start: start + chunk, :], coords_ref[start:start+chunk, :], align)
             print("       ", sub_loss.cpu().item(), coords[start: start + chunk, :].shape)
-            if coords[start: start + chunk, :].shape[0] == 1:
+            if coords[start: start + chunk, :].shape[0] == 1 or sub_loss.cpu().item()>3:
                 print("       ", coords[start: start + chunk, :], coords_ref[start: start + chunk, :])
             loss += sub_loss
             start += chunk
@@ -272,6 +273,9 @@ class Encoder(nn.Module):
         print("[Encoder] prior logvar V", torch.min(prior_logvar_V).item(), torch.max(prior_logvar_V).item()) #, torch.sum(prior_logvar_V,  dim = 1))
         print("[Encoder] prior mean h", torch.min(prior_mean_h).item(), torch.max(prior_mean_h).item(), torch.sum(prior_mean_h).item())
         print("[Encoder] prior logvar h", torch.min(prior_logvar_h).item(), torch.max(prior_logvar_h).item(), torch.sum(prior_logvar_h).item())
+
+        print("\n[Enocoder] prior logvar mlp weight norms", torch.norm(self.prior_logvar_V.linear.weight), torch.norm(self.prior_logvar_V.linear2.weight))
+        print("\n[Enocoder] posterior logvar mlp weight norms", torch.norm(self.posterior_logvar_V.linear.weight), torch.norm(self.posterior_logvar_V.linear2.weight))
         Z_V = self.reparameterize(posterior_mean_V, posterior_logvar_V)
         # if torch.gt(Z_V, 100).any() or  torch.lt(Z_V, -100).any():
         #         ipdb.set_trace()
@@ -305,8 +309,8 @@ class Encoder(nn.Module):
 
     def reparameterize(self, mean, logvar, scale = 1.0):
         # scale = 0.3 # trying this
-        # sigma = 1e-12 + torch.exp(scale*logvar / 2)
-        sigma = 1e-12 + F.softplus(scale*logvar / 2)
+        sigma = 1e-12 + torch.exp(scale*logvar / 2)
+        # sigma = 1e-12 + F.softplus(scale*logvar / 2)
         eps = torch.randn_like(mean)
         # if torch.gt(mean + eps*sigma, 100).any() or  torch.lt(mean + eps*sigma, -100).any():
         #         ipdb.set_trace()
@@ -322,17 +326,37 @@ class Encoder(nn.Module):
         # ipdb.set_trace() #TODO look into softplus instead of exp
         # p_std = 1e-12 + torch.exp(z_logvar / 2)
         # q_std = 1e-12 + torch.exp(z_logvar_prior / 2)
-        p_std = 1e-12 + F.softplus(z_logvar / 2)
-        q_std = 1e-12 + F.softplus(z_logvar_prior / 2)
+        # # p_std = 1e-12 + F.softplus(z_logvar / 2)
+        # # q_std = 1e-12 + F.softplus(z_logvar_prior / 2)
+        # q_mean = z_mean_prior
+        # p_mean = z_mean
+        # var_ratio = (p_std / q_std).pow(2).sum(-1)
+        # t1 = ((p_mean - q_mean) / q_std).pow(2).sum(-1)
+        # if coordinates:
+        #     var_ratio = var_ratio.sum(-1)
+        #     t1 = t1.sum(-1)
+        # kl1 =  0.5 * (var_ratio + t1 - 1 - var_ratio.log()) # shape = number of CG beads
+        # return kl.mean()
+        free_bits = 5
+        free_bits_per_dim = free_bits/z_mean[0].numel()
+
+        p_std = 1e-12 + torch.exp(z_logvar / 2)
+        q_std = 1e-12 + torch.exp(z_logvar_prior / 2)
+        # p_std = 1e-12 + F.softplus(z_logvar / 2)
+        # q_std = 1e-12 + F.softplus(z_logvar_prior / 2)
         q_mean = z_mean_prior
         p_mean = z_mean
-        var_ratio = (p_std / q_std).pow(2).sum(-1)
-        t1 = ((p_mean - q_mean) / q_std).pow(2).sum(-1)
-        if coordinates:
-            var_ratio = var_ratio.sum(-1)
-            t1 = t1.sum(-1)
+        var_ratio = (p_std / q_std).pow(2)
+        t1 = ((p_mean - q_mean) / q_std).pow(2)
         kl =  0.5 * (var_ratio + t1 - 1 - var_ratio.log()) # shape = number of CG beads
+        kl = torch.clamp(kl, min = free_bits_per_dim)
+        # ipdb.set_trace()
+        kl = kl.sum(-1)
+        if coordinates:
+            kl = kl.sum(-1)
         return kl.mean()
+
+        
 
 class Decoder(nn.Module):
     def __init__(self, iegmn, D, F, atom_embedder, device = "cuda", norm="ln", teacher_forcing = True):
@@ -554,8 +578,8 @@ class Decoder(nn.Module):
             src = src.long()
             dst = dst.long()
             d_squared = torch.sum((generated_coords[src] - generated_coords[dst]) ** 2, dim=1)
-            # geom_loss.append(1/len(src) * torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
-            geom_loss.append(torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
+            geom_loss.append(1/len(src) * torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
+            # geom_loss.append(torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
         print("          [AR Distance Loss Step]", geom_loss)
         # if true_coords is not None:
         #     for a, b, c, d in zip (generated_coords_all, geom_loss, true_coords, dgl.unbatch(geometry_graphs)):

legacy_code/vae_delta.py
@@ -55,9 +55,9 @@ class VAE_Delta(nn.Module):
             src = src.long()
             dst = dst.long()
             generated_coords = generated_mol.ndata['x_cc']
-            # d_squared = torch.sum((generated_coords[src] - generated_coords[dst]) ** 2, dim=1)
+            d_squared = torch.sum((generated_coords[src] - generated_coords[dst]) ** 2, dim=1)
             geom_loss.append(1/len(src) * torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2)) #TODO: scaling hurt performance
-            geom_loss.append(torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
+            # geom_loss.append(torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
         print("[Distance Loss]", geom_loss)
         return torch.mean(torch.tensor(geom_loss))
 
@@ -131,6 +131,7 @@ class VAE_Delta(nn.Module):
 
     def std(self, input):
         return 1e-12 + torch.exp(input / 2)
+        #  return 1e-12 + F.softplus(input / 2)
 
     def align(self, source, target):
         # Rot, trans = rigid_transform_Kabsch_3D_torch(input.T, target.T)
@@ -305,8 +306,8 @@ class Encoder(nn.Module):
 
     def reparameterize(self, mean, logvar, scale = 1.0):
         # scale = 0.3 # trying this
-        # sigma = 1e-12 + torch.exp(scale*logvar / 2)
-        sigma = 1e-12 + F.softplus(scale*logvar / 2)
+        sigma = 1e-12 + torch.exp(scale*logvar / 2)
+        # sigma = 1e-12 + F.softplus(scale*logvar / 2)
         eps = torch.randn_like(mean)
         # if torch.gt(mean + eps*sigma, 100).any() or  torch.lt(mean + eps*sigma, -100).any():
         #         ipdb.set_trace()
@@ -320,10 +321,10 @@ class Encoder(nn.Module):
         # loss = (reconstruction( MSE = negative log prob) + self.kl_beta * KL)
         # Dkl( P || Q) # i added the .sum()
         # ipdb.set_trace() #TODO look into softplus instead of exp
-        # p_std = 1e-12 + torch.exp(z_logvar / 2)
-        # q_std = 1e-12 + torch.exp(z_logvar_prior / 2)
-        p_std = 1e-12 + F.softplus(z_logvar / 2)
-        q_std = 1e-12 + F.softplus(z_logvar_prior / 2)
+        p_std = 1e-12 + torch.exp(z_logvar / 2)
+        q_std = 1e-12 + torch.exp(z_logvar_prior / 2)
+        # p_std = 1e-12 + F.softplus(z_logvar / 2)
+        # q_std = 1e-12 + F.softplus(z_logvar_prior / 2)
         q_mean = z_mean_prior
         p_mean = z_mean
         var_ratio = (p_std / q_std).pow(2).sum(-1)
@@ -541,10 +542,10 @@ class Decoder(nn.Module):
                     print("update with reference", atom_ids)
                     progress[batch_idx] += 1
             batch_idx += 1
-        lens = [sum([len(y) for y in x]) for x in ids]
+        # lens = [sum([len(y) for y in x]) for x in ids]
         # check = all([a-b.item() == 0 for a, b in zip(lens,progress)])
         # ipdb.set_trace()
-        assert(check)
+        # assert(check)
         return ids, progress
 
     def distance_loss(self, generated_coords_all, geometry_graphs, true_coords = None):
@@ -553,6 +554,8 @@ class Decoder(nn.Module):
             src, dst = geometry_graph.edges()
             src = src.long()
             dst = dst.long()
+            if len(src) == 0 or len(dst) == 0:
+                continue
             d_squared = torch.sum((generated_coords[src] - generated_coords[dst]) ** 2, dim=1)
             geom_loss.append(1/len(src) * torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
             # geom_loss.append(torch.sum((d_squared - geometry_graph.edata['feat'] ** 2) ** 2))
@@ -594,28 +597,6 @@ class Decoder(nn.Module):
             true = self.align(true, generated)
         loss = self.mse(true, generated)
         return loss
-
-    # def isolate_next_subgraph(self, final_molecule, id_batch, true_geo_batch):
-    #     molecules = dgl.unbatch(final_molecule)
-    #     geos = dgl.unbatch(true_geo_batch)
-    #     result = []
-    #     geo_result = []
-    #     # valid = []
-    #     # check = True
-    #     for idx, ids in enumerate(id_batch):
-    #         if ids is None:
-    #             # valid.append((idx, False))
-    #             # check = False
-    #             continue
-    #         # else:
-    #             # valid.append((idx, True))
-    #         fine = molecules[idx]
-    #         subg = dgl.node_subgraph(fine, ids)
-    #         result.append(subg)
-
-    #         subgeo = dgl.node_subgraph(geos[idx], ids)
-    #         geo_result.append(subgeo)
-    #     return dgl.batch(result).to(self.device), dgl.batch(geo_result).to(self.device) #valid, check
 
     def get_reference(self, subgraph, molecule, ref_ids):
         # references = []
@@ -660,7 +641,7 @@ class Decoder(nn.Module):
         final_molecule = rdkit_mol_graph
         frag_ids = self.sort_ids(cg_frag_ids, bfs_order)
         # print(frag_ids)
-        # frag_ids, progress = self.add_reference(frag_ids, ref_order, progress) #! Do not need reference when we do delta coordinates
+        frag_ids, progress = self.add_reference(frag_ids, ref_order, progress) #! Do not need reference when we do delta coordinates?
         # ipdb.set_trace()
         frag_batch = defaultdict(list) # keys will be time steps
        
@@ -690,8 +671,8 @@ class Decoder(nn.Module):
             id_batch = frag_batch[t]
             r_batch = ref_batch[t]
             # print("ID", id_batch)
-            if t == 1:
-                ipdb.set_trace()
+            # if t == 1:
+            #     ipdb.set_trace()
             latent, geo_latent = self.isolate_next_subgraph(final_molecule, id_batch, true_geo_batch)
             latent = self.get_reference(latent, final_molecule, r_batch)
             # ipdb.set_trace()